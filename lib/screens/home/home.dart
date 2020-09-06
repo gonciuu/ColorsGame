2cb0de044432b26c2030b7f9fe86eb3e5f8a8ff1
andryue <kacperwojak17@gmail.com>
2020-09-06 10:00:39 +0200

    add game button, hightscore and score counter ui

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,12 +11,39 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: <Widget> [
+        alignment: Alignment.center,
+        children: <Widget>[
           Row(
-            children: <Widget> [
-              Flexible(child: Container(color: Colors.amber),fit: FlexFit.tight),
-              Flexible(child: Container(color: Colors.blueAccent),fit: FlexFit.tight)
+            children: <Widget>[
+              Flexible(
+                  child: Container(color: Colors.amber), fit: FlexFit.tight),
+              Flexible(
+                  child: Container(color: Colors.blueAccent),
+                  fit: FlexFit.tight)
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom:30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("0", style: TextStyle(color: Colors.white,
+                    fontSize: 100.0,
+                    fontWeight: FontWeight.bold),),
+                SizedBox(height: 50.0,),
+                GestureDetector(
+                  child: Icon(
+                    Icons.play_circle_outline,
+                    size: 180.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 70.0,),
+                Text("High score:\n23931", style: TextStyle(color: Colors.white,
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
+              ],
+            ),
           ),
         ],
       ),
