79f02410a62a2636cb0fd5ef43f789e966c6c29e
andryue <kacperwojak17@gmail.com>
2020-09-06 09:42:40 +0200

    create basic of home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,6 +8,17 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: <Widget> [
+          Row(
+            children: <Widget> [
+              Flexible(child: Container(color: Colors.amber),fit: FlexFit.tight),
+              Flexible(child: Container(color: Colors.blueAccent),fit: FlexFit.tight)
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
