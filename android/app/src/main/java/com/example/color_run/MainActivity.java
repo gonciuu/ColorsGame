d434b9dbf84ab27905f6b51583dd6743ba5e8fc2
andryue <kacperwojak17@gmail.com>
2020-09-06 09:41:47 +0200

    init project and update gitignore

---
+++ android/app/src/main/java/com/example/color_run/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.color_run;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import './screens/home/home.dart';
+
+void main() =>runApp(ColorRun());
+
+
+class ColorRun extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
