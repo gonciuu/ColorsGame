bf161c5af1435277da0e7b0116a0b24d43b568f7
andryue <kacperwojak17@gmail.com>
2020-09-10 13:21:48 +0200

    change colors on click and choose random ligher color

--- lib/screens/home/colors.dart
+++ lib/screens/home/colors.dart
@@ -12,29 +12,37 @@ class GameColors extends StatefulWidget {
 }
 
 class _GameColorsState extends State<GameColors> {
-
-  double randomDarker() => Random().nextDouble() * (0.05 - 0.015) + 0.015;
+  
+  int randomContainer() => Random().nextInt(2);
+  double randomDarker = Random().nextDouble() * (0.05 - 0.015) + 0.015;
 
   @override
   Widget build(BuildContext context) {
+    final Widget dark = Flexible(
+        child: Stack(
+          children: [
+            Container(color: widget._color),
+            GestureDetector(
+              child: Container(color: Color.fromRGBO(0, 0, 0, randomDarker)),
+              onTap: () {
+                widget._playGame();
+                print(randomContainer());
+              },
+            ),
+          ],
+        ),
+        fit: FlexFit.tight);
+
+    final Widget light = Flexible(child: GestureDetector(child: Container(color: widget._color),onTap: (){
+      widget._playGame();
+    },), fit: FlexFit.tight);
+
+    int colorRandomContainer = randomContainer();
+
     return Row(
       children: <Widget>[
-        Flexible(child: Container(color: widget._color), fit: FlexFit.tight),
-        Flexible(
-            child: Stack(
-              children: [
-                Container(color: widget._color),
-                GestureDetector(
-                  child:
-                      Container(color: Color.fromRGBO(0, 0, 0, randomDarker())),
-                  onTap: () {
-                    widget._playGame();
-                    print("klik");
-                  },
-                ),
-              ],
-            ),
-            fit: FlexFit.tight)
+        colorRandomContainer == 0 ? dark : light,
+        colorRandomContainer == 0 ? light : dark
       ],
     );
   }
