9b26b9b340df438dc5a91f6aff7668f4295c9d1a
andryue <kacperwojak17@gmail.com>
2020-09-12 10:57:33 +0200

    setup project

--- ios/Runner/AppDelegate.h
+++ ios/Runner/AppDelegate.h
@@ -1,6 +1,16 @@
 #import <Flutter/Flutter.h>
 #import <UIKit/UIKit.h>
+import Firebase
 
-@interface AppDelegate : FlutterAppDelegate
+@interface AppDelegate : FlutterAppDelegate{
+ var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @end
