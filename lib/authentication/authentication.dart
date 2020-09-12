d9c892b4deba432d9ec21384703cae459ec73aea
andryue <kacperwojak17@gmail.com>
2020-09-12 12:04:08 +0200

    create method to login and register with email and password with handling exceptions

--- lib/authentication/authentication.dart
+++ lib/authentication/authentication.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 class Authentication{
 
@@ -8,9 +9,25 @@ class Authentication{
 
   Future registerWithEmailAndPassword(String email,String password) async{
     try{
-      await auth.createUserWithEmailAndPassword(email: email, password: password);
+      return await auth.createUserWithEmailAndPassword(email: email, password: password);
     }catch(e){
-      print(e);
+      if(e is PlatformException){
+        return (e).message;
+      }else{
+        return (e).toString();
+      }
+    }
+  }
+
+  Future loginWithEmailAndPassword(String email,String password) async{
+    try{
+      return await auth.signInWithEmailAndPassword(email: email, password: password);
+    }catch(e){
+      if(e is PlatformException){
+        return (e).message;
+      }else{
+        return e.toString();
+      }
     }
   }
 
--- lib/screens/auth/login_form.dart
+++ lib/screens/auth/login_form.dart
@@ -1,12 +1,22 @@
-
 import 'package:color_run/authentication/authentication.dart';
 import 'package:color_run/constants/inputs_decorations.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
 
-class LoginForm extends StatelessWidget {
-
+class _LoginFormState extends State<LoginForm> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
   final Authentication _authentication = Authentication();
+
+  void showBottomSnackBar(Widget content) {
+    Scaffold.of(context).showSnackBar(SnackBar(content: content));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -17,44 +27,60 @@ class LoginForm extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              style: TextStyle(color: Colors.white),
-              decoration:  InputLoginTextDecoration.copyWith(hintText:"Enter Email",hintStyle: TextStyle(color: Colors.grey[400]),)
-            ),SizedBox(height: 15.0,),
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(color: Colors.white),
+                decoration: InputLoginTextDecoration.copyWith(
+                  hintText: "Enter Email",
+                  hintStyle: TextStyle(color: Colors.grey[400]),
+                )),
+            SizedBox(
+              height: 15.0,
+            ),
             TextFormField(
-              obscureText: true,
-              style: TextStyle(color: Colors.white),
-                decoration:  InputLoginTextDecoration.copyWith(hintText:"Enter Password",hintStyle: TextStyle(color: Colors.grey[400]),)
-            ),SizedBox(height: 30.0,),
+                controller: passwordController,
+                obscureText: true,
+                style: TextStyle(color: Colors.white),
+                decoration: InputLoginTextDecoration.copyWith(
+                    hintText: "Enter Password",
+                    hintStyle: TextStyle(color: Colors.grey[400]))),
+            SizedBox(
+              height: 30.0,
+            ),
             Container(
               width: double.infinity,
               child: FlatButton(
                 padding: EdgeInsets.symmetric(vertical: 20.0),
                 color: Color.fromRGBO(244, 13, 193, 1),
-                onPressed: () async{
-                 await  _authentication.registerWithEmailAndPassword("janek@onet.pl", "wejfwhfu213Qa");
+                onPressed: () async {
+                  dynamic user = await _authentication.loginWithEmailAndPassword(emailController.text, passwordController.text);
+                  if (user is String) {
+                    showBottomSnackBar(Text(user));
+                  } else {
+                    print((user as AuthResult).user.uid);
+                  }
                 },
                 child: Text(
                   "Login",
                   style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700
-                  ),
+                      color: Colors.white, fontWeight: FontWeight.w700),
                 ),
               ),
-            ),SizedBox(height: 15.0,),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
             Container(
               width: double.infinity,
               child: FlatButton(
                 padding: EdgeInsets.symmetric(vertical: 20.0),
                 color: Color.fromRGBO(170, 138, 183, 1),
-                onPressed: () {  },
+                onPressed: () {},
                 child: Text(
                   "Login with google",
                   style: TextStyle(
                       color: Color.fromRGBO(34, 12, 44, 1),
-                      fontWeight: FontWeight.w700
-                  ),
+                      fontWeight: FontWeight.w700),
                 ),
               ),
             )
