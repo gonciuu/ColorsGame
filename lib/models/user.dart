08b72e812faf1d3455115f91cf567f25244d68ca
andryue <kacperwojak17@gmail.com>
2020-09-15 20:52:59 +0200

    complete app models

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,15 +1,19 @@
 
+import 'package:flutter/cupertino.dart';
+
 class User {
   String uid;
   String nickName;
+  int highScore;
 
-  User({this.nickName, this.uid});
+  User({@required this.nickName,@required this.uid,@required this.highScore});
 
 
   Map<String, dynamic> userMap() {
     Map<String, dynamic> mapToReturn = Map();
     mapToReturn['uid'] = this.uid;
     mapToReturn['nickName'] = this.nickName;
+    mapToReturn['highScore'] = this.highScore;
     return mapToReturn;
   }
 
@@ -17,6 +21,7 @@ class User {
     return User(
       uid: map['uid'],
       nickName: map['nickName'],
+      highScore: map['highScore'],
     );
   }
 
