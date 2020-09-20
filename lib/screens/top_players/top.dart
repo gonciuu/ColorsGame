86c643b1b03ea878fb286478fc4732774520bbac
andryue <kacperwojak17@gmail.com>
2020-09-20 20:05:39 +0200

    final touches with separating widgets

--- lib/screens/top_players/top.dart
+++ lib/screens/top_players/top.dart
@@ -1,4 +1,5 @@
 import 'package:color_run/models/user.dart';
+import 'package:color_run/screens/top_players/user_list_tile.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,54 +10,7 @@ class Top extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.fromLTRB(
-              0.0, 8.0, 8.0, 8.0),
-          child: ListTile(
-            title: Text(
-              snapshot.data[index].nickName.length > 0
-                  ? snapshot.data[index].nickName
-                  : "Empty nick",
-              style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(
-              backgroundColor:
-              Color.fromRGBO(244, 13, 193, 1),
-              radius: 40.0,
-              child: Center(
-                child: Text(
-                  snapshot.data[index].nickName.length >
-                      0
-                      ? "${snapshot.data[index].nickName[0].toUpperCase() ?? "?"}"
-                      : "?",
-                  style: TextStyle(
-                      fontSize: 36.0,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-            ),
-            trailing: Container(
-              width: 100,
-              child: FittedBox(
-                fit: BoxFit.scaleDown,
-                child: Text(
-                  snapshot.data[index].highScore
-                      .toString(),
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
+        itemBuilder: (context, index) => UserListTile(snapshot,index),
         itemCount: snapshot.data.length,
         shrinkWrap: true,
         scrollDirection: Axis.vertical,
--- lib/screens/top_players/user_list_tile.dart
+++ lib/screens/top_players/user_list_tile.dart
@@ -1,1 +1,61 @@
+import 'package:color_run/models/user.dart';
 import 'package:flutter/material.dart';
+
+class UserListTile extends StatelessWidget {
+
+  final AsyncSnapshot<List<User>> snapshot;
+  final int index;
+
+  UserListTile(this.snapshot,this.index);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(
+          0.0, 8.0, 8.0, 8.0),
+      child: ListTile(
+        title: Text(
+          snapshot.data[index].nickName.length > 0
+              ? snapshot.data[index].nickName
+              : "Empty nick",
+          style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w700,
+              color: Colors.white),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+        leading: CircleAvatar(
+          backgroundColor:
+          Color.fromRGBO(244, 13, 193, 1),
+          radius: 40.0,
+          child: Center(
+            child: Text(
+              snapshot.data[index].nickName.length >
+                  0
+                  ? "${snapshot.data[index].nickName[0].toUpperCase() ?? "?"}"
+                  : "?",
+              style: TextStyle(
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+        trailing: Container(
+          width: 100,
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              snapshot.data[index].highScore
+                  .toString(),
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
