df00d1d8e86bfeed99a50d0fff3931c4e0c5a532
andryue <kacperwojak17@gmail.com>
2020-09-23 19:21:35 +0200

    set preffered device orintations

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,25 @@ import 'package:color_run/authentication/authentication.dart';
 import 'package:color_run/screens/wrapper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import './screens/home/home.dart';
 import './screens/auth/auth.dart';
 
-void main() => runApp(ColorRun());
+void main() {
+  runApp(ColorRun());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+}
 
 class ColorRun extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       home: StreamProvider<FirebaseUser>.value(
         value: Authentication().user,
